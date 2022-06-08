416c53da3ab81eb5efbf3ff826092636f8c6df70
CezzMartins <cezzmartins@gmail.com>
2022-06-08 01:53:31 -0300

    add templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello amigos')
+    return render(request, 'home/home.html')
 
 
 def contact(request):
